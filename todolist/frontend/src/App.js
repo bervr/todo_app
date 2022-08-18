8ba484b393fb0ddfea6ed7e541dda29ad756f0f3
bervr <bervrr@gmail.com>
2022-08-18 18:46:16 +0400

    added projects and items

--- todolist/frontend/src/App.js
+++ todolist/frontend/src/App.js
@@ -3,20 +3,47 @@ import React from 'react';
 import UserList from './components/users'
 import Menu from './components/menu'
 import Footer from './components/footer'
+import Projects from './components/projects'
 import axios from 'axios'
+import TodoItems from "./components/todoitems";
+
+function getDataFromApi(url){
+   return axios.get(url).then(response => response.data.results).catch(error =>console.log(error))
+}
 
 class App extends React.Component {
     constructor(props){
         super(props)
         this.state={
-        'users':[]
+        'users':[],
+        'projects':[],
+        'todoitems':[]
         }
     }
 
+
+
+
 componentDidMount() {
+        // this.setState({'users': getDataFromApi('http://127.0.0.1:8000/api/todousers/')})
+
 axios.get('http://127.0.0.1:8000/api/todousers/').then(response =>{
         this.setState(
-        {'users':response.data}
+        {'users':response.data.results}
+        )
+       }
+    ).catch(error =>console.log(error))
+
+axios.get('http://127.0.0.1:8000/api/projects/').then(response =>{
+        this.setState(
+        {'projects':response.data.results}
+        )
+       }
+    ).catch(error =>console.log(error))
+
+axios.get('http://127.0.0.1:8000/api/todoitems/').then(response =>{
+        this.setState(
+        {'todoitems':response.data.results}
         )
        }
     ).catch(error =>console.log(error))
@@ -26,6 +53,8 @@ axios.get('http://127.0.0.1:8000/api/todousers/').then(response =>{
         <div>
             <Menu/>
             <UserList users = {this.state.users}/>
+            <Projects projects = {this.state.projects}/>
+            <TodoItems todoitems = {this.state.todoitems}/>
             <Footer/>
         </div>
         )
---
+++ todolist/frontend/src/components/projects.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+
+const ProjectItem =({project}) =>{
+    return(
+    <tr>
+        <td>{project.projectName }</td>
+        <td>{project.repoLink}</td>
+        <td>{project.projectOwner}</td>
+        <td>{project.projectGroup}</td>
+    </tr>
+
+    )
+
+}
+
+const Projects =({projects}) => {
+    return(
+    <table class="table table-striped">
+    <thead>
+    <tr>
+    <th scope="col">Project name</th>
+    <th scope="col">Repo link</th>
+    <th scope="col">Project owner</th>
+    <th scope="col">Project group</th>
+    </tr>
+    </thead>
+    <tbody>
+    {projects.map((project) => <ProjectItem project = {project}/>)}
+    </tbody>
+    </table>
+
+    )
+
+}
+
+export default Projects;
