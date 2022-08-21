a829f73d430f717cb482ff2e7a108d379d74e817
bervr <bervrr@gmail.com>
2022-08-21 20:38:48 +0400

    jwt added

--- todolist/todoapp/serializers.py
+++ todolist/todoapp/serializers.py
@@ -4,10 +4,11 @@ from .models import TodoItem, Project
 
 class TodoItemSerializer(serializers.ModelSerializer):
     # item_owner = serializers.ReadOnlyField(source='item_owner.username')
-    # item_project = serializers.ReadOnlyField(source='item_project.project_name')
+    item_project_name = serializers.ReadOnlyField(source='item_project.project_name')
+    item_owner_name = serializers.ReadOnlyField(source='item_owner.username')
     class Meta:
         model = TodoItem
-        fields = ['id', 'note', 'item_project', 'todo_status', 'item_owner', 'created', 'updated']
+        fields = ['id', 'note', 'item_project', 'item_project_name',  'todo_status', 'item_owner', 'item_owner_name', 'created', 'updated']
 
 
 class ProjectSerializer(serializers.ModelSerializer):
